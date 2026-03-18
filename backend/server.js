d25bbd812e62c4e1cf9104e20063be1edbe3d898
sandeeppaticlearpath-cmyk <sandeeppati.clearpath@gmail.com>
2026-03-19 00:07:37 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth');
+const providerRoutes = require('./routes/providers');
+const bobRoutes = require('./routes/bob');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/providers', providerRoutes);
+app.use('/api/bob', bobRoutes);
+
+app.get('/', (req, res) => {
+  res.send("ProviderIQ API running");
+});
+
+app.listen(5000, () => console.log("Server running"));
